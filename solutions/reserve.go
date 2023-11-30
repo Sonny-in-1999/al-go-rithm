db0b939e0e09cd003e3ab12d3f467f08c85e2d84
minsu <minsoo1022@gmail.com>
2023-11-30 14:51:17 +0900

    feat: reserve.go 솔루션 추가

--- solutions/reserve.go
+++ solutions/reserve.go
@@ -3,11 +3,23 @@ package solutions
 import "strings"
 
 func Reserve(word string) string {
-	//var res string
-	//for i := len(word) - 1; i >= 0; i-- {
-	//	res += string(word[i])
-	//}
-	//return res
+	/* // Solution 1.
+	var res string
+	for i := len(word) - 1; i >= 0; i-- {
+		res += string(word[i])
+	}
+	return res
+	*/
+
+	/* // Solution 2
+	var res string
+	for _, w := range word {
+		res = string(w) + res
+	}
+	return res
+	*/
+
+	// solution 3
 	var sb strings.Builder
 	for i := len(word) - 1; i >= 0; i-- {
 		sb.WriteByte(word[i])
